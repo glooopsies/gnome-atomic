f417af2e14879ef43ace801809d3f2f3f4dc11bd
glooopsies <petar@radojevic.rs>
2024-06-16 02:01:19 +0200

    enable tailscaled service

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ RELEASE="$(rpm -E %fedora)"
 
 curl https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
 rpm-ostree install tailscale
+systemctl enable tailscaled.service
 
 systemctl enable podman.socket
